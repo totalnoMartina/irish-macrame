ad744c8fc881516f803956f3f17884b387201827
totalnoMartina <totalnobzvz@yahoo.com>
2022-09-08 23:31:21 +0000

    Fix allowed hosts adding herokuAPP to match name of host

--- makemacrame/settings.py
+++ makemacrame/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['black-moon-design.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['black-moon-design.herokuapp.com', 'localhost']
 
 
 # Application definition
