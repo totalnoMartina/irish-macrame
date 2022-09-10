89b16b5b41fa7d2dbc91ef493a69fb8e26bd27d7
totalnoMartina <totalnobzvz@yahoo.com>
2022-09-10 10:26:22 +0000

    Arranged Admin panel for categories and macrames

--- shoppingapp/admin.py
+++ shoppingapp/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Macrame, Review, Category
 
-admin.site.register(Macrame)
-admin.site.register(Category)
+
+class MacrameAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('-name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Macrame, MacrameAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Review)
--- shoppingapp/views.py
+++ shoppingapp/views.py
@@ -1,5 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponseRedirect
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+from django.views.generic import View
+
 
 def index(request):
     """ A view to return homepage """
     return render(request, 'shoppingapp/index.html')
+
+
+def all_macrames(request):
+    """ A view to show all macrame items, including sorting and search queries """
+
+    macrames = Macrame.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                macrames = macrames.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            macrames = macrames.order_by(sortkey)
+            
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('macrames'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            macrames = macrames.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'macrames': macrames,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+    }
+
+    return render(request, 'shoppingapp/macrames.html', context)
+
+
