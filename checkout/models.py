83365b822d42545dc43208d664cbc0d59a09499a
totalnoMartina <totalnobzvz@yahoo.com>
2022-09-10 21:58:35 +0000

    rendering checkout and checkout_success, not rendering my profile page yet

--- checkout/models.py
+++ checkout/models.py
@@ -8,13 +8,13 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from shoppingapp.models import Macrame
-
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     """ A model for order attributes """
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(User, on_delete=models.SET_NULL,
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -67,8 +67,8 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     """ A model for items in the beg already ordered """
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Macrame, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    macrame = models.ForeignKey(Macrame, null=False, blank=False, on_delete=models.CASCADE)
+    macrame_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
--- checkout/views.py
+++ checkout/views.py
@@ -149,7 +149,6 @@ def checkout_success(request, order_number):
     # Attach the user's profile to the order
     order.user_profile = profile
     order.save()
-
     # Save the user's info
     if save_info:
         profile_data = {
