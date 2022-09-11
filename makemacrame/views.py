0b074ce4181d858bb6a712fa98d26a063cfaac37
totalnoMartina <totalnobzvz@yahoo.com>
2022-09-11 15:39:37 +0000

    Add view for 404

---
+++ makemacrame/views.py
@@ -0,0 +1,8 @@
+
+"""Views to handle 404 page"""
+from django.shortcuts import render
+
+
+def handle_erorr_404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "error404/page404.html", status=404)
