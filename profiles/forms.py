861dbb73ea2993770e62eef231776e27d5c35bc8
totalnoMartina <totalnobzvz@yahoo.com>
2022-09-14 17:42:11 +0000

    Add docstring to form model

--- profiles/forms.py
+++ profiles/forms.py
@@ -3,6 +3,7 @@ from .models import UserProfile
 
 
 class UserProfileForm(forms.ModelForm):
+    """ A form to be presenting information based on UserProfile model """
     class Meta:
         model = UserProfile
         exclude = ('user',)
