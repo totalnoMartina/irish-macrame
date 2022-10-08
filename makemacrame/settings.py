957957f68fa357bb850e5767022888659d4b0179
totalnoMartina <totalnobzvz@yahoo.com>
2022-10-08 09:21:12 +0000

    add False to debug and docstring to settings.py

--- makemacrame/settings.py
+++ makemacrame/settings.py
@@ -1,9 +1,10 @@
+""" Main settings of the application """
 from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-    
+
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -12,15 +13,11 @@ import cloudinary.api
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['black-moon-design.herokuapp.com', 'localhost',
                  'black-moon-design.herokuapp.com/accounts/signup']
