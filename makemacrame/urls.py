35d136f415e485b608dcc5d4666ac774d40dbab1
totalnoMartina <totalnobzvz@yahoo.com>
2022-11-01 14:28:50 +0000

    Remove unused imports in url.py makemacrame

--- makemacrame/urls.py
+++ makemacrame/urls.py
@@ -1,8 +1,6 @@
 """ Main urls for the apps to be connected """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import handle_error_404
 
 
