f10fac81a54ec7f106e53587978b868adc0c6637
totalnoMartina <totalnobzvz@yahoo.com>
2022-11-10 22:00:26 +0000

    Add debug False to see f it all works

--- makemacrame/settings.py
+++ makemacrame/settings.py
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['black-moon-design.herokuapp.com', 'localhost',
                  '127.0.0.1', 'https://8000-totalnomart-irishmacram-2xfjpzurppx.ws-eu75.gitpod.io/']
