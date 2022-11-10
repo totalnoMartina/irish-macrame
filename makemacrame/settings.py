e7bdd9a91ba01645ea52a9bf2d3282afbd5511b9
totalnoMartina <totalnobzvz@yahoo.com>
2022-11-10 21:36:08 +0000

    Add debug true , 400 still there

--- makemacrame/settings.py
+++ makemacrame/settings.py
@@ -17,9 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-# ALLOWED_HOSTS = ['black-moon-design.herokuapp.com', 'localhost',
+
+# ALLOWED_sHOSTS = ['black-moon-design.herokuapp.com', 'localhost',
 #                  '127.0.0.1']
 ALLOWED_HOSTS = ['*', '.example.com']
 print(ALLOWED_HOSTS)
